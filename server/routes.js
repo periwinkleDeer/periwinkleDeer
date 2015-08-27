1ec6397fe4f7448011f9d3fa746d6ade86231413
Kevbyte <kevin.schweigert@gmail.com>
2015-08-27 16:42:14 -0700

    rebase

--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,14 @@
 var food = require('./foodController.js')
 
 module.exports = function (app) {
-//user related routes
-  app.get('/login', );
-  //retrieve user's last dishes
-  app.get('/findoradd', );
-  //rate dishes 
-  app.post('/findoradd', );
-  //user can upload dish
-  app.post('/add', );
+// //user related routes
+//   app.get('/login', );
+//   //retrieve user's last dishes
+//   app.get('/findoradd', );
+//   //rate dishes 
+//   app.post('/findoradd', );
+//   //user can upload dish
+//   app.post('/add', );
 
 //food related routes
   //get all dishes for appetizer, maincourse & dessert
