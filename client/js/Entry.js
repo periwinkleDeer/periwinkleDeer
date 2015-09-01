b30a5c807d4450dfd578f2e0e18c5a6b61dac55b
Marvine Chi <chi.marvine@gmail.com>
2015-08-31 17:18:28 -0700

    star rating functional

--- client/js/Entry.js
+++ client/js/Entry.js
@@ -1,33 +1,33 @@
-var StarRating = require('react-star-rating');
+var Rating = require('react-rating');
 
-function resize (file, maxWidth, maxHeight, fn) {
+function resize(file, maxWidth, maxHeight, fn){
     var reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = function (event) {
+    reader.onload = function(event) {
         var dataUrl = event.target.result;
 
         var image = new Image();
         image.src = dataUrl;
-        image.onload = function () {
+        image.onload = function(){
             var resizedDataUrl = resizeImage(image, maxWidth, maxHeight, 0.7);
             fn(resizedDataUrl);
         };
     };
 }
 
-function resizeImage(image, maxWidth, maxHeight, quality) {
+function resizeImage(image, maxWidth, maxHeight, quality){
     var canvas = document.createElement('canvas');
 
     var width = image.width;
     var height = image.height;
 
-    if (width > height) {
-        if (width > maxWidth) {
+    if(width > height){
+        if(width > maxWidth){
             height = Math.round(height * maxWidth / width);
             width = maxWidth;
         }
     } else {
-        if (height > maxHeight) {
+        if(height > maxHeight){
             width = Math.round(width * maxHeight / height);
             height = maxHeight;
         }
@@ -42,9 +42,10 @@ function resizeImage(image, maxWidth, maxHeight, quality) {
 }
 
 var Entry = React.createClass({
-   getInitialState: function () {
+   getInitialState: function(){
        return {};
    },
+
    componentDidMount: function(){
       var self = this;
       var input = document.getElementById('restaurant');
@@ -54,7 +55,7 @@ var Entry = React.createClass({
 
       var autocomplete = new google.maps.places.Autocomplete(input, options);
 
-      autocomplete.addListener('place_changed', function() {
+      autocomplete.addListener('place_changed', function(){
         var place = autocomplete.getPlace();
 
         console.log(place);
@@ -65,7 +66,7 @@ var Entry = React.createClass({
       });
    },
 
-   handleSubmit: function() {
+   handleSubmit: function(){
       var store = {
          restaurant : this.state.restaurant.name,
          address    : this.state.restaurant.formatted_address,
@@ -73,8 +74,8 @@ var Entry = React.createClass({
          phone      : this.state.restaurant.formatted_phone_number,
          rating     : this.state.restaurant.rating,
          dishName   : document.getElementById('dish').value,
-         dishRating : document.getElementById('dishRating').value,
-         dishPrice  : document.getElementById('dishPrice').value,
+         dishRating : this.foodRate,
+         dishPrice  : this.priceRate,
          imgUrl     : this.state.dataUrl
       };
 
@@ -94,7 +95,7 @@ var Entry = React.createClass({
        return false;
    },
 
-   onChange: function (e) {
+   onChange: function(e){
        var files = e.target.files;
        var self = this;
        var maxWidth = this.props.maxWidth;
@@ -104,21 +105,36 @@ var Entry = React.createClass({
        });
    },
 
-   render: function () {
+   priceRate: function(value){
+      this.priceRate = value;
+      var values = {
+         1: "under $10",
+         2: "$10 - $20",
+         3: "$20 - $30",
+         4: "$30 - $40"
+      };
+      $('.popup').text(values[value]);
+   },
+
+   foodRate: function(value){
+      this.foodRate = value;
+   },
+
+   render: function(){
       var style = {
-        color: 'red',
+        color: 'white',
         fontSize: 13
       };
 
        var image;
 
        var dataUrl = this.state.dataUrl;
-       if (dataUrl) {
-           image = <img src={dataUrl} />
+       if(dataUrl){
+         image = <img src={dataUrl}/>
        }
 
        return (
-          <div className="container">
+        <div className="container">
            <div className="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
 
              <div className="form-group">
@@ -133,18 +149,9 @@ var Entry = React.createClass({
 
              <div className="form-group">
                  <label for="exampleInputFile">Upload Picture</label>
-                 <input style={style} ref="upload" type="file" accept="image/*" onSubmit={this.handleSubmit} onChange={ this.onChange }/>
+                 <input style={style} ref="upload" type="file" accept="image/*" onSubmit={this.handleSubmit} onChange={this.onChange}/>
                  <p className="help-block">{ image }</p>
              </div>           
-
-             <div className="form-group">
-             <label>Price Rating</label>
-               <select id="dishPrice" className="form-control">
-                 <option>$</option>
-                 <option>$$</option>
-                 <option>$$$</option>
-               </select>
-             </div>
 
              <div className="form-group">
              <label>Category</label>
@@ -156,8 +163,16 @@ var Entry = React.createClass({
              </div>
 
              <div className="form-group">
+              <label>Price Rating</label>
+              <div className="rating" id="rating-fontawesome-star"></div>
+                 <Rating empty="glyphicon glyphicon-usd usd" full="glyphicon glyphicon-usd green usd" start={0} stop={4} step={1} onChange={this.priceRate}/>
+                 <span className="popup"></span>
+             </div>
+
+             <div className="form-group">
               <label>Rate Your Foodie!</label>
-              <p id="star-rating"></p>
+              <div className="rating" id="rating-fontawesome-star"></div>
+                 <Rating empty="glyphicon glyphicon-star-empty star" full="glyphicon glyphicon-star orange star" start={0} stop={5} step={1} onChange={this.foodRate}/>
              </div>
 
                <button className="btn btn-warning form-control" onClick={this.handleClick}>Share My Thoughts!</button>
@@ -170,11 +185,11 @@ var Entry = React.createClass({
 
 var Test = React.createClass({
 
-    onChange: function (file) {
+    onChange: function(file){
         console.log('done', file);
     },
 
-    render: function () {
+    render: function(){
         return <div>
             <Entry className="entry" maxHeight={250} maxWidth={250} onChange={ this.onChange } />
         </div>
