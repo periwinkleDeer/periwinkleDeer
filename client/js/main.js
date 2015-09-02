f22e868bdab3cc8445ec385af73fef9e02678faa
jeremyhu9 <jeremyhu415@gmail.com>
2015-09-01 19:20:40 -0700

    Added missing content

--- client/js/main.js
+++ client/js/main.js
@@ -11,6 +11,7 @@ var Main = React.createClass({
     return {dishes: 'Loading...'}
   },
   componentDidMount: function() {
+    this.ratings = {};
     var self = this;
     $.ajax({
         url: "/unrated",
@@ -19,19 +20,18 @@ var Main = React.createClass({
         success: function(data) {
           console.log(data, 'posting');
           var dishes = [
-            {'name': 'Sisig', 'rating': 4},
-            {'name': 'Ravioli', 'rating': 2},
-            {'name': 'Deep Dish Pizza', 'rating': 3}
+          {'id': 1, 'name': 'Sisig', 'rating': 4},
+          {'id': 2, 'name': 'Ravioli', 'rating': 2},
+          {'id': 3, 'name': 'Deep Dish Pizza', 'rating': 3}
           ];
-
-          console.log(dishes);
 
           self.setState({dishes: 
             dishes.map(function(dish) {
               return (
               <div className="card">
                 <div>{dish.name}</div>
-                <Rating empty="glyphicon glyphicon-star-empty star" full="glyphicon glyphicon-star orange star" start={0} stop={5} step={1} onChange={self.foodRate}/>
+                <Rating empty="glyphicon glyphicon-star-empty star" full="glyphicon glyphicon-star orange star" start={0} stop={5} step={1} onChange={self.foodRate.bind(null, dish)}/>
+                <span id={dish.id} className="glyphicon glyphicon-remove remove" onClick={self.handleRemove.bind(null, dish)}></span>
               </div>
                 );
             })
@@ -49,31 +49,66 @@ var Main = React.createClass({
       }
     })
   },
-  foodRate: function(value) {
-    console.log(value);
+  handleRemove: function(dish) { 
+    $('#' + dish.id).parent().hide('slow');
+    this.ratings[dish.id] = -1;
+  },
+  foodRate: function(dish, value) {
+    this.ratings[dish.id] = value;
+  }, 
+  handleSubmit: function() {
+    var self = this;
+    var query = {id: this.props.query.id};
+    query.dishes = [];
+    for (var prop in this.ratings) {
+      query.dishes.push({id: prop, rating: this.ratings[prop]})
+    }
+    console.log(query)
+    $.ajax({
+      method: 'GET',
+      url: '/rate',
+      data: query,
+      success: function(data) {
+        console.log('yay!', data);
+        self.componentDidMount();
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });
   },
 
-
   handleClick: function(link) {
-    console.log("btn click", this);
     this.context.router.transitionTo('/' + link);
   },
   render: function() {
-    var self = this;
-    return (
-      <div className="container">
-        <div className="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
-          <div className="form-group">
-            <button type="button" className="btn btn-warning btn-lg btn-block" onClick={this.handleClick.bind(this, "parameters")}>Search for Foodie!</button>
-            <button type="button" className="btn btn-warning btn-lg btn-block spacing" onClick={this.handleClick.bind(this, "entry")}>Share Foodie!</button>
-          </div>
-          <div>
-            {this.state.dishes}
+      var self = this;
+      var submit;
+      if (this.state.dishes.length) {
+        submit = 
+          <div className="center-block">
+            <button className="center-block btn btn-warning" onClick={this.handleSubmit}>
+              Submit Ratings
+            </button>
+          </div>;
+      } else {
+        submit = '';
+      }
+      return (
+        <div className="container">
+          <div className="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
+            <div className="form-group">
+              <button type="button" className="btn btn-warning btn-lg btn-block" onClick={this.handleClick.bind(this, "parameters")}>Search for Foodie!</button>
+              <button type="button" className="btn btn-warning btn-lg btn-block spacing" onClick={this.handleClick.bind(this, "entry")}>Share Foodie!</button>
+            </div>
+            <div>
+              {this.state.dishes}
+            </div>
+            {submit}
           </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
 });
 
 module.exports = Main;
