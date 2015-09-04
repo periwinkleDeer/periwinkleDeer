8361d2d52c12e2ecc7402f8986dd03d8aef4750d
Kevbyte <kevin.schweigert@gmail.com>
2015-09-04 15:50:29 -0700

    query dishes /db

--- server/routes.js
+++ server/routes.js
@@ -16,7 +16,7 @@ module.exports = function(app) {
 //Heroku deployment
   app.get('/db', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM User', function(err, result) {
+      client.query('SELECT * FROM Dishes', function(err, result) {
         done();
         if (err)
          { console.error(err); response.send("Error " + err); }
